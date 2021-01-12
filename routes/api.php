cc0c7eac9e19809dd35b32c0a0b3714b9a802c99
JOBIN JOSE <70318413+Jobin-Jas@users.noreply.github.com>
2021-01-11 20:10:57 -0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\TodoController;
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get("task", [TodoController::class,'task']);
     Route::post("addtask", [TodoController::class,'addtask']);
-    Route::put("update/{task_id}", [TodoController::class,'update']);
+    Route::put("update", [TodoController::class,'update']);
     Route::delete("delete/{task_id}", [TodoController::class,'delete']); 
 });
 Route::post('/register',[UserController::class,'register']);
